dcb2032b7829651cc1fe2fd3476bfa18b1d8b9e8
AntoCic <anto.cic.127@gmail.com>
2024-08-05 18:07:01 +0200

    OK FIEXD

--- functions/function.js
+++ functions/function.js
@@ -14,22 +14,6 @@ const {
   FIREBASE_DATABASE_URL,
 } = process.env
 
-// Inizializza Firebase Admin
-// const serviceAccount = {
-//   "type": "service_account",
-//   "project_id": FIREBASE_PROJECT_ID,
-//   "private_key_id": FIREBASE_PRIVATE_KEY_ID,
-//   "private_key": FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//   "client_email": FIREBASE_CLIENT_EMAIL,
-//   "client_id": FIREBASE_CLIENT_ID,
-//   "auth_uri": FIREBASE_AUTH_URI,
-//   "token_uri": FIREBASE_TOKEN_URI,
-//   "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-//   "client_x509_cert_url": FIREBASE_CLIENT_X509_CERT_URL,
-//   "universe_domain": FIREBASE_UNIVERSE_DOMAIN
-// };
-
-
 const serviceAccount = {
   "type": "service_account",
   "project_id": FIREBASE_PROJECT_ID,
