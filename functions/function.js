8f5a4989dec24d27a7be79e50cacdbe744c661c1
AntoCic <anto.cic.127@gmail.com>
2024-08-05 17:57:47 +0200

    prova on

--- functions/function.js
+++ functions/function.js
@@ -1,12 +1,52 @@
 import admin from 'firebase-admin';
 
+const {
+  FIREBASE_PROJECT_ID,
+  FIREBASE_PRIVATE_KEY_ID,
+  FIREBASE_PRIVATE_KEY,
+  FIREBASE_CLIENT_EMAIL,
+  FIREBASE_CLIENT_ID,
+  FIREBASE_AUTH_URI,
+  FIREBASE_TOKEN_URI,
+  FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  FIREBASE_CLIENT_X509_CERT_URL,
+  FIREBASE_UNIVERSE_DOMAIN,
+  FIREBASE_DATABASE_URL,
+} = process.env
+
 // Inizializza Firebase Admin
+// const serviceAccount = {
+//   "type": "service_account",
+//   "project_id": FIREBASE_PROJECT_ID,
+//   "private_key_id": FIREBASE_PRIVATE_KEY_ID,
+//   "private_key": FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+//   "client_email": FIREBASE_CLIENT_EMAIL,
+//   "client_id": FIREBASE_CLIENT_ID,
+//   "auth_uri": FIREBASE_AUTH_URI,
+//   "token_uri": FIREBASE_TOKEN_URI,
+//   "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+//   "client_x509_cert_url": FIREBASE_CLIENT_X509_CERT_URL,
+//   "universe_domain": FIREBASE_UNIVERSE_DOMAIN
+// };
+
+
 const serviceAccount = {
-  
+  "type": "service_account",
+  "project_id": FIREBASE_PROJECT_ID,
+  "private_key_id": FIREBASE_PRIVATE_KEY_ID,
+  "private_key": FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": FIREBASE_CLIENT_EMAIL,
+  "client_id": FIREBASE_CLIENT_ID,
+  "auth_uri": FIREBASE_AUTH_URI,
+  "token_uri": FIREBASE_TOKEN_URI,
+  "auth_provider_x509_cert_url": FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": FIREBASE_CLIENT_X509_CERT_URL,
+  "universe_domain": FIREBASE_UNIVERSE_DOMAIN
 };
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://name-list-8296b-default-rtdb.europe-west1.firebasedatabase.app',
+  databaseURL: FIREBASE_DATABASE_URL,
 });
 
 const db = admin.database();
